ada74866eb62cf4c74f3552cd16d03524b652cc6
wonbin <wjdqkseo@gmail.com>
2022-07-14 02:04:29 +0900

    main page 0713

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from './Loading.scss';
 import classNames from 'classnames/bind';
 
+const cx = classNames.bind(styles);
+
 const Loading: React.FC = () => {
   return (
     <div className={styles.loading}>
       <span className={styles.icon_loading}>
-        <span className={classnames('blind')}>loading</span>
+        <span className={cx('blind')}>loading</span>
       </span>
     </div>
   );
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -9,9 +9,31 @@ const cx = classNames.bind(styles);
 
 const Main: React.FC<IOwnProps> = ({ }) => {
   return (
-    <>
-      <div className={cx('main')}>main</div>
-    </>
+    <div className={cx('main')}>
+      <div className={cx('header')}>
+        <div className={cx('header_img_wrap')}>
+          <img className={cx('header_img')}>
+          </img>
+        </div>
+        <div className={cx('header_title_wrap')}>
+          <img src="/assets/img/logo-white.svg"></img>
+        </div>
+        <button className={cx('menu_wrap')}>
+          <img></img>
+        </button>
+        <div className={cx('header_text_wrap')}>
+          <strong>당신의 새로운 일상을 이어갑니다.</strong>
+        </div>
+        <div className={cx('background_Pagenation_wrap')}>
+          <div className={cx('background_Pagenation')} aria-selected="true"></div>
+          <div className={cx('background_Pagenation')} aria-selected="false"></div>
+          <div className={cx('background_Pagenation')} aria-selected="false"></div>
+        </div>
+      </div>
+      <div className={cx('')}>
+
+      </div>
+    </div>
   );
 };
 
