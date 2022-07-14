fd6ef398ef4f2ef9e05e39e93c812147dae3bed3
wonbin <wjdqkseo@gmail.com>
2022-07-15 01:11:39 +0900

    main page 0714 오후 모바일

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -18,9 +18,7 @@ const Main: React.FC<IOwnProps> = ({ }) => {
         <button className={cx('menu_wrap')}>
           <img ></img>
         </button>
-        <div className={cx('header_text_wrap')}>
-          <strong>당신의 새로운 일상을 이어갑니다.</strong>
-        </div>
+        <strong className={cx('header_text_wrap')}>당신의 새로운 일상을 이어갑니다.</strong>
         <div className={cx('background_pagenation_wrap')}>
           <div className={cx('background_pagenation')} aria-selected="true"></div>
           <div className={cx('background_pagenation')} aria-selected="false"></div>
@@ -52,11 +50,104 @@ const Main: React.FC<IOwnProps> = ({ }) => {
           <div className={cx('background_pagenation')} aria-selected="false"></div>
         </div>
       </div>
-      <div className={cx('kt_cloud_news_wrap')}>
-
-
+      <strong className={cx('kt_cloud_news')}>
+        KT Cloud가
+        <br />
+        이룬 소식을 전합니다.
+      </strong>
+      <ul className={cx('news_wrap')}>
+        <li className={cx('news_contents_wrap')}>
+          <a className={cx('news_img_wrap')} href=''>
+            <img className={cx('news_img')} />
+          </a>
+          <p className={cx('news_title')}>KT 클라우드, 출정식 개최…클라우드·IDC 역량 결집</p>
+          <p className={cx('news_date')}>2022.06.08</p>
+        </li>
+        <li className={cx('news_contents_wrap')}>
+          <a className={cx('news_img_wrap')} href=''>
+            <img className={cx('news_img')} />
+          </a>
+          <p className={cx('news_title')}>KT, 클라우드 생태계 상생 확대, 장터 입점 기업 수익 보전</p>
+          <p className={cx('news_date')}>2022.06.08</p>
+        </li>
+        <li className={cx('news_contents_wrap')}>
+          <a className={cx('news_img_wrap')} href=''>
+            <img className={cx('news_img')} />
+          </a>
+          <p className={cx('news_title')}>KT-틸론, DaaS 플랫폼 공동개발 MOU 체결</p>
+          <p className={cx('news_date')}>2022.06.08</p>
+        </li>
+        <li className={cx('news_contents_wrap')}>
+          <a className={cx('news_img_wrap')} href=''>
+            <img className={cx('news_img')} />
+          </a>
+          <p className={cx('news_title')}>KT, 소상공인시장진흥공단 클라우드 전환사업 수주</p>
+          <p className={cx('news_date')}>2022.06.08</p>
+        </li>
+        <li className={cx('news_contents_wrap')}>
+          <a className={cx('news_img_wrap')} href=''>
+            <img className={cx('news_img')} />
+          </a>
+          <p className={cx('news_title')}>개방형 공공 클라우드 KT 파스타, CSAP 획득</p>
+          <p className={cx('news_date')}>2022.06.08</p>
+        </li>
+        <li className={cx('news_contents_wrap')}>
+          <a className={cx('news_img_wrap')} href=''>
+            <img className={cx('news_img')} src='/assets/img/temp2.png' />
+          </a>
+          <p className={cx('news_title')}>KT 2분기 DX 선전 이끈 클라우드의 힘</p>
+          <p className={cx('news_date')}>2022.06.08</p>
+        </li>
+      </ul>
+      <a href='#' className={cx('learn_link')}>자세히 알아보기</a>
+      <div className={cx('career_esg_wrap')}>
+        <div className={cx('career_esg')}>
+          <img className={cx('career_esg_img')} src='/assets/img/temp2.png' />
+          <a className={cx('career_esg_link')}>
+            <span>Career</span>
+          </a>
+          <p className={cx('career_esg_text')}>
+            우리의 열정으로
+            <br />
+            세상이 성장하고 있습니다
+          </p>
+        </div>
+        <div className={cx('career_esg')}>
+          <img className={cx('career_esg_img')} src='/assets/img/temp2.png' />
+          <a className={cx('career_esg_link')}>
+            <span>ESG</span>
+          </a>
+          <p className={cx('career_esg_text')}>
+            기업과 사회가 공존하는
+            <br />
+            지속가능한 미래
+          </p>
+        </div>
       </div>
-    </div>
+      <div className={cx('footer')}>
+        <div className={cx('service_policy_wrap')}>
+          <a className={cx('service_policy')}>개인정보 처리방침</a>
+          <a className={cx('service_policy')}>서비스 이용약관</a>
+          <a className={cx('service_policy')}>윤리위반 신고</a>
+        </div>
+        <div className={cx('company_wrap')}>
+          <strong className={cx('company')}>주식회사 케이티클라우드</strong>
+          <div className={cx('company_info_wrap')}>
+            <span className={cx('company_info')}>대표이사 윤동식</span>
+            <span className={cx('company_info')}>서울특별시 강남구 학동로 343 더피나클타워</span>
+            <span className={cx('company_info')}>사업자등록번호 : 696-87-02611 </span>
+            <span className={cx('company_info')}>고객센터 : 080-2580-005</span>
+            <span className={cx('company_info')}>이메일 : ktcloud-voc@kt.com</span>
+          </div>
+        </div>
+        <div className={cx('company_name')}>
+          <img src='/assets/img/logo-black.svg' />
+        </div>
+        <a href='#' className={cx('cooperation_company')}>
+          <span>관계사 링크</span>
+        </a>
+      </div>
+    </div >
   );
 };
 
