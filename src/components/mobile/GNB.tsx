ee10635d60c006bf8b2175ef61f279dec6b6c37d
wonbin <wjdqkseo@gmail.com>
2022-07-17 21:27:13 +0900

    gnb 작업중

--- src/components/mobile/GNB.tsx
+++ src/components/mobile/GNB.tsx
@@ -11,7 +11,42 @@ const GNB: React.FC<IOwnProps> = ({
 
 }) => {
   return (
-    <div className={cx('gnb')}>GNB</div>
+    <div className={cx('gnb')}>
+      <div className={cx('page_wrap')}>
+        <div className={cx('header_wrap')}>
+          <div className={cx('header_img')} />
+          <button className={cx('close_img')} />
+        </div>
+        <ul className={cx('menu_list_wrap')}>
+          <li className={cx('menu_list')}>
+            <strong className={cx('menu_title')}>기업소개</strong>
+            <div className={cx('menu_link_wrap')}>
+              <a className={cx('menu_link')}>소개</a>
+              <a className={cx('menu_link')}>윤리 · 컴플라이언스</a>
+            </div>
+          </li>
+          <li className={cx('menu_list')}>
+            <a className={cx('menu_title')}>연혁</a>
+          </li>
+          <li className={cx('menu_list')}>
+            <a className={cx('menu_title')}>회사 정보</a>
+          </li>
+          <li className={cx('menu_list')}>
+            <a className={cx('menu_title')}>ESG 경영</a>
+          </li>
+          <li className={cx('menu_list')}>
+            <a className={cx('menu_title')}>주요 관계사</a>
+          </li>
+          <li className={cx('menu_list')}>
+            <a className={cx('menu_title')}>
+              인재채용
+              <div className={cx('link_bnt')} />
+            </a>
+
+          </li>
+        </ul>
+      </div>
+    </div >
   );
 };
 
--- src/components/mobile/Header.tsx
+++ src/components/mobile/Header.tsx
@@ -13,18 +13,19 @@ interface IOwnProps {
 };
 
 const Header: React.FC<IOwnProps> = ({ isLight }) => {
-  const [showGNB, setShowGNB] = useState<boolean>(false);
+  const [showGNB, setShowGNB] = useState<boolean>(true);
   const { useScroll } = useScrollFixed();
 
   const useFixed = location.pathname !== URLInfo.INTRODUCE_HISTORY && useScroll;
 
   return (
-    <div className={cx('header', useFixed && 'fixed', isLight && 'light')}>
-      <Link to={URLInfo.MAIN} className={cx('header_title_wrap')}>
-      </Link>
-      <button className={cx('menu_wrap')} onClick={() => setShowGNB(true)}>
-
-      </button>
+    <div className={cx('header')}>
+      <div className={cx('page_wrap', useFixed && 'fixed', isLight && 'light')}>
+        <Link to={URLInfo.MAIN} className={cx('header_title_wrap')}>
+        </Link>
+        <button className={cx('menu_wrap')} onClick={() => setShowGNB(true)}>
+        </button>
+      </div>
       {showGNB && (
         <GNB />
       )}
